c1a91b5f6af99fad523090ae005e6eb001060de0
mark-sprott <sprotty01@gmail.com>
2018-05-10 16:42:21 +1200

    prevention component rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,46 +2,49 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Tile from './Tile'
 import Header from './Header'
+import Prevention from './Prevention'
 // import Whale from './Whale'
 
-const App = () => (
+const App = () => {
+  return (
+    <Router>
+      <div>
 
-  <div>
-        <Router>
-          <div className='container'>
-            <Route exact path="/" render={
-              () => {
-                return (<Header title="Embracing Blue !!!" />)
-              }
-            } />
-          </div>
-        </Router>
+        {/* header */}
+        <div className='container'>
+          <Route exact path="/" render={
+            () => {
+              return (<Header title="Embracing Blue !!!" />)
+            }
+          } />
+        </div>
 
-    {/* this whale is responsive and hardcoded; this is not calling a component */}
+          {/* Prevention */}
+        <div className="resources">
+          <Route path = "/"   component = {Prevention} />
+        </div>
 
-        <Router>
-          <div>
-            <div>
-                <img className="u-full-width" src="backgrounds/1.png"/>
-            </div>
-          </div>  
-        </Router>
+        {/* this whale is responsive and hardcoded; this is not calling a component */}
+  
+        <div>
+          <img className="u-full-width" src="backgrounds/1.png"/>
+        </div>
 
+        {/* this is where we're trying to get the 3 colums to render, still not working but currently slightly aligned  */}
+  
+        <div className='container'>
+          <div class="row">
+            <div class="twelve columns">
+              <div class="six columns offset-by-five">
+                  <h1>3</h1>
+                  <h1>3</h1>
+                  <h1>3</h1>
+                </div>
+              </div>
+          </div>      
+        </div>
 
-      {/* this is where we're trying to get the 3 colums to render, still not working but currently slightly aligned  */}
-      <div className='container'>
-      <div class="row">
-        <div class="twelve columns">
-          <div class="six columns offset-by-five">
-              <h1>3</h1>
-              <h1>3</h1>
-              <h1>3</h1>
-            </div>
-            </div>
-      </div>      
-      </div>
-
-    {/* priya's original tiles/links: */}
+        {/* priya's original tiles/links: */}
 
         <div>
           <Tile src="images/pexels-photo-697243.jpeg" linkText="Young" />
@@ -55,7 +58,9 @@ const App = () => (
           <Tile src="images/pexels-photo-939702.jpeg" linkText="Multicultural" />
         </div>
 
-  </div>
-)
+      </div>
+    </Router>
+  )
+}
 
 export default App
