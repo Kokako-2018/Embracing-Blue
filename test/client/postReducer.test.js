a0e30828adfa29d554021fe2dd90c1d9a4bfc4cc
Rosie Burkett <burkettroz@gmail.com>
2018-05-10 19:41:14 +1200

    posts actions and reducers done. Some tests written for both

---
+++ test/client/postReducer.test.js
@@ -0,0 +1,72 @@
+import postsReducer from '../../client/reducers/posts'
+
+test('Reducer Initial State', () => {
+  const expected = []
+
+  const actual = postsReducer(undefined, {})
+
+  expect(actual).toEqual(expected)
+})
+
+
+test('RECEIVE_ALL_POSTS case', () => {
+  const fakePosts = [
+    'This is a fake post',
+    'What do you know? Another fake post'
+  ]
+
+  const expected = [...fakePosts]
+
+  const action = {
+    type: 'RECEIVE_ALL_POSTS',
+    posts: fakePosts
+  }
+
+  const actual = postsReducer(undefined, action)
+
+  expect(actual).toEqual(expected)
+})
+
+
+
+// test('ADD_CAT case', () => {
+//   const initialState = [
+//     {post: 'This is a mega cool post'}
+//   ]
+
+//   const post = {
+//     post: 'This post is here to be added'
+//   }
+
+//   const expectedState = [
+//     {post: 'This is a mega cool post'},
+//     post
+//   ]
+
+//   const actual = postsReducer(initialState, action)
+//   expect(actual).toEqual(expectedState)
+//   expect(actual[1]).toBe(post)
+// })
+
+
+test('delete cat case', () => {
+  const post = {
+    content: 'this post is gona be deleted'
+  }
+
+  const action = {
+    type: 'DELETE_POST',
+    post
+  }
+
+  const initialState = [
+    post
+  ]
+
+  const expected = []
+
+  const actual = postsReducer(initialState, action)
+
+  expect(actual).toHaveLength(initialState.length-1)
+  expect(actual.find(actualPost => actualPost.content == post.content)).toBeFalsy()
+})
