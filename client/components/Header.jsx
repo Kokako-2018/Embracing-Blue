f14509bb412036805a2454700a54e8d2503b4a99
Annika <liebste.anni@gmail.com>
2018-05-11 16:13:03 +1200

    Tried to change navBar on home page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,14 +5,45 @@ import { Link } from 'react-router-dom'
 class Header extends React.Component {
     render() {
       return (
-        
+        <div>
           <header className="header">
-            <nav>
-              <div>
+            <nav className="navbar">
+              <div className="container">
+                <ul className="navBar-list">
+                  <li className="navBar-item">
+                    <Link className ="navBar-link" to="/">Home</Link>
+                  </li>
+
+                  <li className="navBar-item">                 
+                    <Link className ="navBar-link" to="/">Our Stories</Link>
+                  </li>
+
+                  <li className="navBar-item">                 
+                    <Link className ="navBar-link" to="/posts">Forum</Link>
+                  </li>
+
+                  <li className="navBar-item">                
+                    <Link className ="navBar-link" to="/posts">Register</Link>
+                  </li> 
+                </ul>
+              </div>  
+             </nav>
+            </header>
+          </div>     
+      )
+    }
+  }
+  
+  export default Header;
+  
+
+          {/* <li className="navBar-item">
+                    <a className="navBar-link" href="#home">Home</a>
+                  </li>
                 <h1 className="header-title">
                   {this.props.title}
                 </h1>
-              </div>  
+          
               <div>
                 <ul >
                   <li className="navlink">
@@ -27,17 +58,5 @@ class Header extends React.Component {
                   <li className="navlink">
                     <Link to="/Register">Register</Link>
                   </li>
-                </ul>
-              </div>  
-              {/* <img src={pots} className="Header-pots" alt="header" /> */}
-            </nav>
-          </header>
-          
-        
-      );
-    }
-  }
-  
-  export default Header;
-  
+                </ul> */}
   
