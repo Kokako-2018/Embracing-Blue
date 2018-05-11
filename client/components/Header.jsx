3e6fb8b1f7c4318fca53b93e99b1aa87569c7bff
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-05-11 19:15:21 +1200

    fixed header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -39,38 +39,9 @@ class Header extends React.Component {
                   </li>
 
                   <li className="navBar-item">                
-                    <Link className ="navBar-link" to="/posts">Register</Link>
+                    <Link className ="navBar-link" to="/register">Register</Link>
                   </li> 
-                </ul>
-              </div>  
-             </nav>
-            </header>
-          </div>     
-      )
-    }
-  }
-  
-  export default Header;
-  
 
-          {/* <li className="navBar-item">
-                    <a className="navBar-link" href="#home">Home</a>
-                  </li>
-                <h1 className="header-title">
-                  {this.props.title}
-                </h1>
-          
-              <div>
-                <ul >
-                  <li className="navlink">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li className="navlink">
-                    <Link to="/our stories">our stories</Link>
-                  </li>
-                  <li className="navlink">
-                    <Link to="/posts">Forum</Link>
-                  </li>
                   {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
                   {!auth.isAuthenticated 
                     ? [
@@ -85,23 +56,21 @@ class Header extends React.Component {
                         <Link to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
                       </li>
                   }
-                  
-                 
-               </ul>
+
+                </ul>
               </div>  
-              {/* <img src={pots} className="Header-pots" alt="header" /> */}
-            </nav>
-          </header>
-          
-    
-      );
+             </nav>
+            </header>
+          </div>     
+      )
     }
   }
-
-  const mapStateToProps = ({auth}) => ({auth})
+  
+              
+const mapStateToProps = ({auth}) => ({auth})
 
   
-  export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header)
   
 
   
