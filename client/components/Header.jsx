034d670012905816f3adb987f47ec548c3971140
Annette <annettegraham56@gmail.com>
2018-05-12 14:41:40 +1200

    merge to master

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,6 +5,7 @@ import Login from './Login'
 import {logoutUser} from '../actions/logout'
 
 class Header extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
@@ -12,56 +13,67 @@ class Header extends React.Component {
     }
     this.toggleBurger = this.toggleBurger.bind(this)
   }
+
   toggleBurger() {
     this.setState({showBurger: !this.state.showBurger})
   }
-    render() {
 
-      const {auth} = this.props 
-      const {showBurger} = this.state
+  render() {
 
-      return (
-        <div>
-          <header className="header">
-            <nav className="navbar">
-              <div className="container">
-                <ul className="navBar-list">
-                  <li className="navBar-item">
-                    <Link className ="navBar-link" to="/">Home</Link>
-                  </li>
+    const {auth} = this.props 
+    const {showBurger} = this.state
 
-                  <li className="navBar-item">                 
-                    <Link className ="navBar-link" to="/ourStories">Our Stories</Link>
-                  </li>
+    return (
+      
+        <header className="header">
+          
+            
+              
+            <section className="hero is-primary">
+              <div className="hero-body">
+                <div className="container has-text-centered">
+                  <h1 className="title is-1">{this.props.title}</h1>
+                </div>
+              </div>
+            </section>  
 
-                  <li className="navBar-item">                 
-                    <Link className ="navBar-link" to="/posts">Forum</Link>
-                  </li>
+            <nav className="navbar has-background-grey-lighter justify-center">
+              
+                  <Link className="navbar-item" to="/">Home</Link>                
+                  <Link className="navbar-item" to="/ourStories">Our Stories</Link>                
+                  <Link className="navbar-item" to="/posts">Forum</Link>
+                
 
-                  <li className="navBar-item">                
-                    <Link className ="navBar-link" to="/register">Register</Link>
-                  </li> 
+                {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
+                {!auth.isAuthenticated 
+                  ? [
+                      <Link key={1} className="navbar-item" to="/register">Register</Link>,
+                      <Link key={2} className="navbar-item" to="/login">Login</Link>
+                      
+                    ]
+                    : 
+                      <Link className="navbar-item" to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
+                    
+                }
+                
+                <div className="field has-addons navbar-item">
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Find a story" />
+                  </div>
+                  <div className="control">
+                    <a className="button is-info">
+                      Search
+                    </a>
+                  </div>
+                </div>
 
-                  {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
-                  {!auth.isAuthenticated 
-                    ? [
-                        <li key={1} className="navlink">
-                          <Link to="/register">Register</Link>
-                        </li>,
-                        <li key={2} className="navlink">
-                          <Link to="/login">Login</Link>
-                        </li>
-                      ]
-                    : <li className="navlink">
-                        <Link to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
-                      </li>
-                  }
-
-                </ul>
-              </div>  
-             </nav>
-            </header>
-          </div>     
+              
+              </nav>
+              
+            
+            
+          </header>
+        
       )
     }
   }
--- client/components/Resources.jsx
+++ client/components/Resources.jsx
@@ -5,24 +5,20 @@ import { Link } from 'react-router-dom'
 const Resources = () => {
     
         return  (
-            <div>
-                <div className='infoButton'>
-                    <Link to="/anxiety">
+            <div className="columns">
+                
+                    <Link className="column" to="/anxiety">
                         <img src = 'clickables/1.png'/>
                     </Link>
-                </div>
-
-                <div className='infoButton'>
-                    <Link to="/depression">
+                
+                    <Link className="column" to="/depression">
                         <img src = 'clickables/2.png'/>
                     </Link>
-                </div>
-
-              <div className='infoButton'>
-                  <Link to="/prevention">
+                
+                  <Link className="column" to="/prevention">
                       <img src = 'clickables/3.png'/>
                   </Link>
-              </div>
+              
             </div> 
         )
 }
