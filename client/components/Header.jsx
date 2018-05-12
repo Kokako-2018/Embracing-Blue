e4f33d2efcfebea11578ce112e35d76fdf69d2b7
Annette <annettegraham56@gmail.com>
2018-05-12 17:35:41 +1200

    have re-ordered some components

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import { Router, Route, Link } from 'react-router-dom'
-import Login from './Login'
+import Login from './auth/Login'
 import {logoutUser} from '../actions/logout'
 
 class Header extends React.Component {
@@ -20,42 +20,42 @@ class Header extends React.Component {
 
   render() {
 
-    const {auth} = this.props 
+    const {auth} = this.props
     const {showBurger} = this.state
 
     return (
-      
+
         <header className="header">
-          
-            
-              
+
+
+
             <section className="hero is-primary">
               <div className="hero-body">
                 <div className="container has-text-centered">
                   <h1 className="title is-1">{this.props.title}</h1>
                 </div>
               </div>
-            </section>  
+            </section>
 
             <nav className="navbar has-background-grey-lighter justify-center">
-              
-                  <Link className="navbar-item" to="/">Home</Link>                
-                  <Link className="navbar-item" to="/ourStories">Our Stories</Link>                
+
+                  <Link className="navbar-item" to="/">Home</Link>
+                  <Link className="navbar-item" to="/ourStories">Our Stories</Link>
                   <Link className="navbar-item" to="/posts">Forum</Link>
-                
+
 
                 {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
-                {!auth.isAuthenticated 
+                {!auth.isAuthenticated
                   ? [
                       <Link key={1} className="navbar-item" to="/register">Register</Link>,
                       <Link key={2} className="navbar-item" to="/login">Login</Link>
-                      
+
                     ]
-                    : 
+                    :
                       <Link className="navbar-item" to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
-                    
+
                 }
-                
+
                 <div className="field has-addons navbar-item">
                   <div className="control">
                     <input className="input" type="text" placeholder="Find a story" />
@@ -67,22 +67,19 @@ class Header extends React.Component {
                   </div>
                 </div>
 
-              
+
               </nav>
-              
-            
-            
+
+
+
           </header>
-        
+
       )
     }
   }
-  
-              
+
+
 const mapStateToProps = ({auth}) => ({auth})
 
-  
-export default connect(mapStateToProps)(Header)
-  
 
-  
+export default connect(mapStateToProps)(Header)
