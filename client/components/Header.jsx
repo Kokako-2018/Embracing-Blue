5e6e147e52bd24a91ee381dec070f63c74336801
Rosie Burkett <burkettroz@gmail.com>
2018-05-14 11:10:37 +1200

    Auth logged in

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { Router, Route, Link } from 'react-router-dom'
 import Login from './auth/Login'
-import {logoutUser} from '../actions/logout'
+import { logoutUser } from '../actions/logout'
 
 class Header extends React.Component {
 
@@ -15,80 +15,80 @@ class Header extends React.Component {
   }
 
   toggleBurger() {
-    this.setState({showBurger: !this.state.showBurger})
+    this.setState({ showBurger: !this.state.showBurger })
   }
 
   render() {
 
-    const {auth} = this.props
-    const {showBurger} = this.state
+    const { auth } = this.props
+    const { showBurger } = this.state
 
     return (
 
-        <header className="header">
+      <header className="header">
 
 
-            <section className="hero is-primary">
+        <section className="hero is-primary">
 
-              <div className="hero-body">
-                {!auth.isAuthenticated ? '' : <p className="is-pulled-left">Welcome {auth.user.user_name}</p>}
-              </div>
-            </section>
+          <div className="hero-body">
+            {auth.isAuthenticated && <p className="is-pulled-left">Welcome {auth.user.user_name}</p>}
+          </div>
+        </section>
 
-            <section className="hero is-light is-bold">
-              <div className="hero-body">
-                <div className="container has-text-centered">
-                  <h1 className="title is-1">{this.props.title}</h1>
-                  <p className="subtitle is-3">A mental health resource for all Kiwis</p>
-                </div>
-              </div>
-            </section>
-
-
-
-            <nav className="navbar has-background-grey-lighter justify-center">
-
-                  <Link className="navbar-item" to="/">Home</Link>
-                  <Link className="navbar-item" to="/ourStories">Our Stories</Link>
-                  <Link className="navbar-item" to="/posts">Forum</Link>
+        <section className="hero is-light is-bold">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title is-1">{this.props.title}</h1>
+              <p className="subtitle is-3">A mental health resource for all Kiwis</p>
+            </div>
+          </div>
+        </section>
 
 
-                {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
-                {!auth.isAuthenticated
-                  ? [
-                      <Link key={1} className="navbar-item" to="/register">Register</Link>,
-                      <Link key={2} className="navbar-item" to="/login">Login</Link>
 
-                    ]
-                    :
-                      <Link className="navbar-item" to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
+        <nav className="navbar has-background-grey-lighter justify-center">
 
-                }
+          <Link className="navbar-item" to="/">Home</Link>
+          <Link className="navbar-item" to="/ourStories">Our Stories</Link>
+          <Link className="navbar-item" to="/posts">Forum</Link>
 
-                <div className="field has-addons navbar-item">
-                  <div className="control">
-                    <input className="input" type="text" placeholder="Find a story" />
-                  </div>
-                  <div className="control">
-                    <a className="button is-info tabindex=8">
-                      Search
+
+          {/* //auth.... is checking whether someone is logged in or not. If they've registered it will execute the code after the : otherwise will execute code after ?  */}
+          {!auth.isAuthenticated
+            ? [
+              <Link key={1} className="navbar-item" to="/register">Register</Link>,
+              <Link key={2} className="navbar-item" to="/login">Login</Link>
+
+            ]
+            :
+            <Link className="navbar-item" to="/" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
+
+          }
+
+          <div className="field has-addons navbar-item">
+            <div className="control">
+              <input className="input" type="text" placeholder="Find a story" />
+            </div>
+            <div className="control">
+              <a className="button is-info tabindex=8">
+                Search
                     </a>
-                  </div>
-                </div>
+            </div>
+          </div>
 
 
-              </nav>
+        </nav>
 
 
 
-          </header>
+      </header>
 
-      )
-    }
+    )
   }
+}
 
 
-const mapStateToProps = ({auth}) => ({auth})
+const mapStateToProps = ({ auth }) => ({ auth })
 
 
 export default connect(mapStateToProps)(Header)
