bc649e115a76b7649fea4b48dd02e8ed6051568d
Rosie Burkett <burkettroz@gmail.com>
2018-05-14 11:33:28 +1200

    confirm logout v2

--- client/components/ConfirmLogout.jsx
+++ client/components/ConfirmLogout.jsx
@@ -4,34 +4,45 @@ import {connect} from 'react-redux'
 
 import {logoutUser} from '../actions/logout'
 
-const ConfirmLogout = ({logout, close}) => {
-
-  return (
-    <div className="modal is-active">
-    <div className="modal-background"></div>
-      <div className="modal-card">
-        <div className="modal-card-head">
-          <div className="modal-card-title has-text-centered">Are you sure?</div>
-          <button onClick={close} className="delete" aria-label="close"></button>
-        </div>
-        <div className="modal-card-body">
-          <div className="columns">
-            <button onClick={close} className="button is-large column is-6">Cancel</button>
-            <button onClick={() => logout()} className="button is-large column is-6 is-warning">Logout</button>
+class ConfirmLogout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedOut: false
+    }
+  }
+  logout() {
+    this.props.dispatch(logoutUser())
+    this.setState({loggedOut: true})
+  }
+  render() {
+    const {close} = this.props
+    const {loggedOut} = this.state
+    return (
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <div className="modal-card-head">
+            <div className="modal-card-title has-text-centered">Are you sure?</div>
+            <button onClick={close} className="delete" aria-label="close"></button>
+          </div>
+          <div className="modal-card-body">
+            {loggedOut
+              ? <div className="has-text-centered">
+                <p className="subtitle is-2">You are Logged Out</p>
+                <button onClick={close} className="button is-success is-fullwidth is-large">Close</button>
+              </div>
+              : <div className="columns">
+                <button onClick={close} className="button is-large column is-6">Cancel</button>
+                <button onClick={this.logout.bind(this)} className="button is-large column is-6 is-warning">Logout</button>
+              </div>  
+            }
           </div>
         </div>
       </div>
-    </div>
-  )
-}
-
-const mapDispatchToProps = (dispatch, props) => {
-  return {
-    logout: () => {
-      dispatch(logoutUser())
-      props.close()
-    }
+    )
   }
 }
 
-export default connect(null, mapDispatchToProps)(ConfirmLogout)
+
+export default connect()(ConfirmLogout)
