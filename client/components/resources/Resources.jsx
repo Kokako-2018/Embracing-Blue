692b92ceafbb305e134645d888b055897c2dd204
Annette <annettegraham56@gmail.com>
2018-05-14 19:16:58 +1200

    indenting

--- client/components/resources/Resources.jsx
+++ client/components/resources/Resources.jsx
@@ -5,24 +5,20 @@ import { Link } from 'react-router-dom'
 const Resources = () => {
 
         return  (
-            <div>
-                <img className="u-full-width" src="banners/frontpageBanner1.png" />
- 
-                <div className="columns">
+            <div className="columns">
 
-                        <Link className="resources column" to="/anxiety">
-                            <img src = 'clickables/1.png'/>
-                        </Link>
-
-                        <Link className="resources column" to="/depression">
-                            <img src = 'clickables/2.png'/>
-                        </Link>
-
-                    <Link className="resources column" to="/prevention">
-                        <img src = 'clickables/3.png' />
+                    <Link className="resources column" to="/anxiety">
+                        <img src = 'clickables/1.png'/>
                     </Link>
 
-                </div>
+                    <Link className="resources column" to="/depression">
+                        <img src = 'clickables/2.png'/>
+                    </Link>
+
+                  <Link className="resources column" to="/prevention">
+                      <img src = 'clickables/3.png'/>
+                  </Link>
+
             </div>
         )
 }
