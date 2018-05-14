bde1a342fb8384fd034db057227dfc63d24d86f2
Theta Likins <thetalikins@gmail.com>
2018-05-15 10:23:42 +1200

    women, men, and youth banners

--- client/components/identities/Women.jsx
+++ client/components/identities/Women.jsx
@@ -8,6 +8,15 @@ export default class Women extends React.Component {
             <figure className="image">
             <img src ='https://images.law.com/contrib/content/uploads/sites/401/2017/12/Depressed-business-woman-Article-201712281924.jpg'/>
             </figure>
+
+
+             <div className="has-text-centered">
+                    <h3 className="subtitle is-size-3">2016 saw the highest rates of suicide for Kiwi women</h3>
+                    <h1 className="title is-size-1">
+                        <span className="has-text-info">ever recorded.</span> 
+                    </h1>
+                </div>
+                
             <div className="section">
                 <h4 id='paras' className="is-size-3"><b>Women</b></h4>
                 <p id='paras' className="is-size-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
