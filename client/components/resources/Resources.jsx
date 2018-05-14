e480f99dc36f8713101fc3251d59c3e7ad1edf35
Theta Likins <thetalikins@gmail.com>
2018-05-14 16:16:12 +1200

    banner

--- client/components/resources/Resources.jsx
+++ client/components/resources/Resources.jsx
@@ -5,20 +5,24 @@ import { Link } from 'react-router-dom'
 const Resources = () => {
 
         return  (
-            <div className="columns">
+            <div>
+                <img className="u-full-width" src="banners/frontpageBanner1.png" />
+ 
+                <div className="columns">
 
-                    <Link className="resources column" to="/anxiety">
-                        <img src = 'clickables/1.png'/>
+                        <Link className="resources column" to="/anxiety">
+                            <img src = 'clickables/1.png'/>
+                        </Link>
+
+                        <Link className="resources column" to="/depression">
+                            <img src = 'clickables/2.png'/>
+                        </Link>
+
+                    <Link className="resources column" to="/prevention">
+                        <img src = 'clickables/3.png' />
                     </Link>
 
-                    <Link className="resources column" to="/depression">
-                        <img src = 'clickables/2.png'/>
-                    </Link>
-
-                  <Link className="resources column" to="/prevention">
-                      <img src = 'clickables/3.png'/>
-                  </Link>
-
+                </div>
             </div>
         )
 }
