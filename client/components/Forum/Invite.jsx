495043fd980fa023375eae4af89415dbf81445e9
Theta Likins <thetalikins@gmail.com>
2018-05-15 15:59:15 +1200

    forum advert link renders

---
+++ client/components/Forum/Invite.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+export default class Invite extends React.Component {
+    render () {
+        return  (
+    
+            <div className="has-text-centered">
+                    <Link className="title is-size-1 has-text-info"  to="/posts">Join our Forum</Link>
+                <div className="has-text-centered">
+                    <h3 className="subtitle is-size-3">to share stories, ask questions, and get support.</h3>
+                </div>
+            </div>
+        )
+    }
+}
